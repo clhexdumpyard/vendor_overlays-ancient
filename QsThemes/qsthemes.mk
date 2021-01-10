4b6c556a647015232a17b3f4fadf72ebc0f6e1db
eldainosor <eldainosor@gmail.com>
2021-01-10 23:54:34 +0700

    overlays: Add more qs tiles styles [2/2]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- QsThemes/qsthemes.mk
+++ QsThemes/qsthemes.mk
@@ -3,4 +3,9 @@ PRODUCT_PACKAGES += \
     QStileCircleTrim \
     QStileDefault \
     QStileDualToneCircle \
-    QStileSquircleTrim
+    QStileSquircleTrim \
+    QStileAttemptMountain \
+    QStileDottedCircle \
+    QStileNinja \
+    QStilePokesign \
+    QStileWavey
