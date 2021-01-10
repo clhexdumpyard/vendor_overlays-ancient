a2ac4a5c6ef5e992376e5f4ca808af2e153f9981
Genkzsz11 <genkzsz11@gmail.com>
2021-01-11 00:20:23 +0700

    overlays: add navigation radius [3/3]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- config.mk
+++ config.mk
@@ -4,6 +4,9 @@ include vendor/overlays/QsThemes/qsthemes.mk
 # Include Themes files
 include vendor/overlays/themes/themes.mk
 
+# Include QsThemes files
+include vendor/overlays/navbar/navbar.mk
+
 # fonts
 PRODUCT_PACKAGES += \
     FontAppleMintOverlay \
---
+++ navbar/navbar.mk
@@ -0,0 +1,5 @@
+# navbar
+PRODUCT_PACKAGES += \
+    GesturalNavigationRadiusLow \
+    GesturalNavigationRadiusVeryLow \
+    GesturalNavigationRadiusHidden
