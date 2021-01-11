7b934d67d8794468feedda17b01780b2b808db05
AlexCruz <alexcruz@gmail.com>
2021-01-11 13:17:04 +0700

    overlays: Introduce navbar styles [3/3]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- navbar/navbar.mk
+++ navbar/navbar.mk
@@ -3,3 +3,10 @@ PRODUCT_PACKAGES += \
     GesturalNavigationRadiusLow \
     GesturalNavigationRadiusVeryLow \
     GesturalNavigationRadiusHidden
+
+# Navbar Custom
+PRODUCT_PACKAGES += \
+    NavbarAsusOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarTecnoCamonOverlay
