b166ec0de60ce3d67a9505f5b93b81893e268c04
xyzuan <i.xyzuan@gmail.com>
2021-01-15 14:16:32 +0700

    overlays: AncientUI: adapt ThemesUtils with our UI. [2/2]
    - fixup SystemOverlay appear in accent category
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- themes/themes.mk
+++ themes/themes.mk
@@ -1,16 +1,22 @@
 # Themes styles
 PRODUCT_PACKAGES += \
+    SolarizedDarkSettingsOverlay \
     SolarizedDarkSystemOverlay \
     SolarizedDarkSystemUIOverlay \
+    BakedGreenSettingsOverlay \
     BakedGreenSystemOverlay \
     BakedGreenSystemUIOverlay \
+    ChocoXSettingsOverlay\
     ChocoXSystemOverlay \
     ChocoXSystemUIOverlay \
     PitchBlackSystemOverlay \
     PitchBlackSystemUIOverlay \
+    DarkGreySettingsOverlay \
     DarkGreySystemOverlay \
     DarkGreySystemUIOverlay \
+    MaterialOceanSettingsOverlay \
     MaterialOceanSystemOverlay \
     MaterialOceanSystemUIOverlay \
+    AncientClearSettingsOverlay \
     AncientClearSystemOverlay \
     AncientClearSystemUIOverlay
